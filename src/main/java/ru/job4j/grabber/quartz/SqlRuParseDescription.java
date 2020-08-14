0284cb31b7787a33e5fd3648edb6e84956d6b158
xyligan2222 <xyligan2222@yandex.ru>
2020-08-14 11:41:23 +0300

    2.3. Загрузка деталей поста. [#307899] fixed method parsePage.

--- src/main/java/ru/job4j/grabber/quartz/SqlRuParseDescription.java
+++ src/main/java/ru/job4j/grabber/quartz/SqlRuParseDescription.java
@@ -13,13 +13,9 @@ import java.util.List;
 public class SqlRuParseDescription {
 
     public static void main(String[] args) throws Exception {
-
-
-        for (int i = 1; i <= 5; i++) {
-            List<String> url = listUrl("https://www.sql.ru/forum/job-offers/" + i);
-            List<Timestamp> page = getDate("https://www.sql.ru/forum/job-offers/" + i);
-            List<String> description = descriptionVacancy("https://www.sql.ru/forum/job-offers/" + i);
-        }
+        parsePage("https://www.sql.ru/forum/job-offers/", 5);
+       // listUrl("https://www.sql.ru/forum/job-offers/");
+        //descriptionVacancy("https://www.sql.ru/forum/job-offers/");
     }
 
     public static List<String> listUrl(String url) throws IOException {
@@ -47,26 +43,33 @@ public class SqlRuParseDescription {
         return list;
     }
 
-    /*public static List<String> parsePage(String url, int page) throws IOException {
+    public static void parsePage(String url, int page) throws IOException {
         List<Post> posts = new ArrayList<>();
         for (int i = 1; i <= page; i++) {
             List<String> urlList = listUrl(url + i);
-            List<Timestamp> pageList = getDate(url + i);
+            List<Timestamp> dateList = getDate(url + i);
             List<String> descriptionList = descriptionVacancy(url + i);
-           posts.add(new )
+            for (int j = 0; j < urlList.size(); j++) {
+                posts.add(new Post(urlList.get(j), descriptionList.get(j), dateList.get(j)));
+            }
         }
-    } */
+        for (int i = 0; i < posts.size(); i++) {
+            System.out.println(posts.get(i) + posts.get(i).getDescription() + posts.get(i).getCreateDate());
+        }
+
+    }
 
     public static List<String> descriptionVacancy(String string) throws IOException {
             List<String> list = listUrl(string);
-            List<String> description = listUrl(string);
+            List<String> description = new ArrayList<>();
         for (String lists : list) {
             Document doc = Jsoup.connect(lists).get();
             Element row = doc.getElementsByClass("msgBody").get(1);
             Element row2 = doc.select(".msgTable").get(0).getElementsByTag("td").get(0);
             description.add(row2.text() + System.lineSeparator() + row.text());
-            System.out.println(row2.text() + System.lineSeparator() + row.text());
+           // System.out.println(row2.text() + System.lineSeparator() + row.text());
         }
+
         return description;
     }
 
